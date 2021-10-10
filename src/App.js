a53e87d8f7697add9e7ebbf4c0ed53137086f924
Soul-Remix <soulremix-webdev@pm.me>
2021-10-10 15:33:43 +0300

    install react query & add data fetching to main page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import { QueryClientProvider, QueryClient } from 'react-query';
 
 import Navbar from './component/navbar/navbar';
 
 import MainPage from './pages/main/mainPage';
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
     <>
       <Navbar />
-      <Route exact path="/">
-        <MainPage />
-      </Route>
+      <QueryClientProvider client={queryClient}>
+        <Route exact path="/">
+          <MainPage />
+        </Route>
+      </QueryClientProvider>
     </>
   );
 }
--- src/pages/main/mainPage.js
+++ src/pages/main/mainPage.js
@@ -1,11 +1,75 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useQuery } from 'react-query';
 
 import Card from '../../component/card/card';
+import Loader from '../../component/loader/loader';
 
 import './mainPage.css';
 
+const fetchPosts = async (pageNum) => {
+  try {
+    const res = await fetch(
+      `https://guarded-bayou-18266.herokuapp.com/api/v1/posts?page=${pageNum}`
+    );
+    return await res.json();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
 const MainPage = () => {
-  return <div className="container"></div>;
+  const [page, setPage] = useState(1);
+  const [posts, setPosts] = useState([]);
+  const { isLoading, isError, data, error } = useQuery(
+    ['posts', page],
+    () => fetchPosts(page),
+    { keepPreviousData: true }
+  );
+
+  useEffect(() => {
+    if (data) {
+      setPosts((old) => old.concat(data.posts));
+    }
+  }, [data]);
+
+  if (isLoading) {
+    return (
+      <div className="center-container">
+        <Loader />
+      </div>
+    );
+  }
+
+  if (isError) {
+    return <h1>{error}</h1>;
+  }
+
+  if (data) {
+    const hasMore = data.totalItems / 10 > page;
+    const postsArr = posts.map((x) => {
+      return (
+        <Card
+          title={x.title}
+          image={x.image}
+          comments={x.comments}
+          key={x._id}
+        />
+      );
+    });
+    return (
+      <div className="container">
+        {postsArr}
+        {hasMore && (
+          <button
+            className="more-button"
+            onClick={() => setPage((old) => (old += 1))}
+          >
+            Load More
+          </button>
+        )}
+      </div>
+    );
+  }
 };
 
 export default MainPage;
